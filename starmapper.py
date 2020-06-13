bac532f79dac59cda41cc97726b6f0f1ed7c4842
sirius <siriusastrebe@gmail.com>
2020-06-13 02:31:21 -0700

    Added a limited option for use in restricted POV cameras.

--- starmapper.py
+++ starmapper.py
@@ -1,6 +1,13 @@
 import numpy
 import csv
 import prettytable
+import json
+import argparse
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-l", "--limited", help="only display stars in one direction", action="store_true")
+args = parser.parse_args()
 
 column_names = ['StarID','Hip','HD','HR','Gliese','BayerFlamsteed','ProperName','RA','Dec','Distance','Mag','AbsMag','Spectrum','ColorIndex']
 
@@ -14,12 +21,33 @@ r.pop(0) # Remove label row
 stars = sorted(r, reverse=False, key=lambda x: float(x[column_names.index('Mag')]));
 
 
+subset = stars[:1000]
+
 table = prettytable.PrettyTable(column_names)
-for row in stars[0:90]:
+for row in subset[0:90]:
+
     table.add_row(row)
 
 print(table)
 
+
+
+cleaned = []
+for row in subset:
+    if args.limited and (((float(row[7]) > 10 or float(row[7]) < 4)) and float(row[8]) < 45):
+        continue
+
+
+    cleaned.append({'Mag': row[10], 'Name': row[6], 'RA': row[7], 'Dec': row[8], 'ColorIndex': row[13]})
+
+
+with open('stars.json', 'w') as json_file:
+    json.dump(cleaned,  json_file)
+
+
+with open('stars.js', 'w') as js_file:
+    dumps = json.dumps(cleaned)
+    js_file.write("var stars = " + dumps + "\nexport default stars;")
 
 
 
